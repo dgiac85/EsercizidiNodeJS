51c55b46a3e6eb9114c9f03451dd63bc4f0dca77
dgiac85 <dgiac85@gmail.com>
2015-11-24 19:06:41 +0100

    added comments in first script

--- Chapter04/01_simple_server.js
+++ Chapter04/01_simple_server.js
@@ -1,14 +1,14 @@
 
 var http = require('http');
 
-function handle_incoming_request(req, res) {
+function handle_incoming_request(req, res) { //handle_incoming_request è il nome della funzione in ascolto sulla 8080
     console.log("INCOMING REQUEST: " + req.method + " " + req.url);
     res.writeHead(200, { "Content-Type" : "application/json" });
-    res.end(JSON.stringify( { error: null }) + "\n");
+    res.end("\n"+JSON.stringify( { error: 'null' }) + "\n");
 }
 
 
-var s = http.createServer(handle_incoming_request);
+var server = http.createServer(handle_incoming_request);
 
-s.listen(8080);
+server.listen(8080);
 
